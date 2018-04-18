b9d01edb30ef8f4832228aa63120afe1eb3aabb1
1714290026 <1714290026@SJB8-D17.academico.iesb.br>
2018-04-18 20:04:48 -0300

    aula13

--- src/aula13/TestaRotulo.java
+++ src/aula13/TestaRotulo.java
@@ -1,18 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package aula13;
 
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-/**
- *
- * @author 1714290026
- */
 public class TestaRotulo {
 
     public static void main(String[] args) {
